b86b3258faa6a1f8256c5be445609f72eeced370
David <blancdavid34@gmail.com>
2022-07-28 17:36:01 +0200

    Modification du fichier task.component.html modif

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -5,11 +5,24 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css']
 })
-export class TaskComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class TaskComponent  {
+  todos = [
+    {
+      txt: "Faire la vaiselle",
+      completed: true
+    },
+    {
+      txt: "Faire les courses",
+      completed: true
+    },
+    {
+      txt: "Nourrir les chats",
+      completed: true
+    },
+    {
+      txt: "Arroser les plantes",
+      completed: false
+    }
+  ]
 
 }
